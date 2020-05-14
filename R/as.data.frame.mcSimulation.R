9c759982f8eb81ee946e3bb0e5e61d1ccdbd138f
CWWhitney <whitney.cory@gmail.com>
2020-05-14 10:36:46 +0200

    git 'rebase' with master branch and incorporate the changes for stringsAsFactors for R 4.0

--- R/as.data.frame.mcSimulation.R
+++ R/as.data.frame.mcSimulation.R
@@ -9,7 +9,13 @@
 #' @seealso \code{\link{as.data.frame}}
 #' @export
 as.data.frame.mcSimulation <- function(x, row.names = NULL, optional = FALSE, ..., 
-                                       stringsAsFactors = default.stringsAsFactors()){
+                                       stringsAsFactors = NA){
+  
+  if(is.na(stringsAsFactors)) {
+    stringsAsFactors <-
+      if(getRversion() < "4.1.0") default.stringsAsFactors()
+    else FALSE}
+  
   as.data.frame(list(y=x$y,x=x$x), row.names = row.names, optional = optional, ..., 
                 stringsAsFactors = stringsAsFactors)
 }
