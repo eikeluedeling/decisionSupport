c2460e52ee68d188fb7a41203cb0be3055081a24
CWWhitney <whitney.cory@gmail.com>
2020-06-08 11:06:25 +0200

    Clean and add facet to cashflow plot.

--- R/global_variables.R
+++ R/global_variables.R
@@ -1,3 +1,3 @@
 utils::globalVariables(c('x_scale', 'value', 'p5', 
                          'p95', 'p75', 'p50', 'p25', 
-                         'name', 'silly_variable'))
+                         'name', 'decision_option'))
