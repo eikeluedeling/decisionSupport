2eb1c8600eb98c07b0a8aa9f2ecb1ed84ebc93cb
CWWhitney <whitney.cory@gmail.com>
2020-07-20 10:11:18 +0200

    Update and finalize 2 vignettes, shift folder structure for vignettes

--- vignettes/example_decision_function.R
+++ vignettes/example_decision_function.R
@@ -1,42 +1,55 @@
-library(tidyverse)
+## ---- include = F------------------------------------------------------------------------------------------------------------------------------------
+#set global options for knitr chunks 
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>", 
+  fig.width=5, 
+  fig.height=3.5
+)
+
+## ---- warning = F, include = F-----------------------------------------------------------------------------------------------------------------------
+#Automatically write R package citation entries to a .bib file
+knitr::write_bib(c(.packages(), 
+                   'chillR',
+                   'dplyr',
+                   'ggplot2', 
+                   'kableExtra',
+                   'decisionSupport'), 'packages.bib')
+require(tidyverse)
+require(kableExtra)
 library(decisionSupport)
-library(ggplot2)
 
-# A function to create variables ####
-# This is used for running parts of the model line by line within the function 
-# before running the whole MC
+## ----install_packages, eval = FALSE------------------------------------------------------------------------------------------------------------------
+#  install.packages("tidyverse")
+#  
+#  library(tidyverse)
 
-make_variables <- function(est, n = 1)
-{
-  x <- random(rho = est, n = n)
-  for (i in colnames(x))
-    assign(i, as.numeric(x[1, i]), envir = .GlobalEnv)
-}
-
-make_variables(estimate_read_csv("vignettes/vignette_mcSimulation/example_input_table.csv"))
-
+## ----model-------------------------------------------------------------------------------------------------------------------------------------------
 example_decision_function <- function(x, varnames){
-  # ex-post risk: impacts the benefits ####
-  # None included yet
-  # ex-ante risk: impacts the implementation of interventions ####
+  
+  # calculate ex-ante risks: impact the implementation of interventions ####
   intervention_NonPopInvolvEvent <-
     chance_event(intervention_NonPopInvolv, 1, 0, n = 1)
   
-  # calculation of common random draws for all intervention model runs ####
+  # pre-calculation of common random draws for all intervention model runs ####
   
+  # profits from Tropical Livestock Units (TLU)
   TLU <- vv(TLU_no_intervention, var_CV, n_years)
   TLU_profit <- vv(profit_per_TLU, var_CV, n_years)
   
+  # benefits of fruit
   precalc_intervention_fruit_benefits <-
     vv(intervention_fruit_area_ha, var_CV, n_years) *
     vv(intervention_fruit_yield_t_ha, var_CV, n_years) *
     vv(intervention_fruit_profit_USD_t, var_CV, n_years)
   
+  # benefits of vegetables
   precalc_intervention_vegetable_benefits <-
     vv(intervention_vegetable_area_ha, var_CV, n_years) *
     vv(intervention_vegetable_yield_t_ha, var_CV, n_years) *
     vv(intervention_vegetable_profit_USD_t, var_CV, n_years)
   
+  # benefits of rainfed crops
   precalc_intervention_rainfed_crop_benefits <-
     vv(intervention_rainfed_crop_area_ha, var_CV, n_years) *
     vv(intervention_rainfed_crop_yield_t_ha, var_CV, n_years) *
@@ -117,24 +130,16 @@ example_decision_function <- function(x, varnames){
     TLU_intervention <- TLU
   
   if (decision_intervention_strips){
-  
     livestock_benefits <- TLU_intervention * TLU_profit
-    
     total_benefits <- crop_production + livestock_benefits
-    
     net_benefits <- total_benefits - intervention_cost
-    
     result_interv <- net_benefits}
   
   
   if (!decision_intervention_strips){
-    
     livestock_benefits <- TLU_no_intervention * TLU_profit
-    
     total_benefits <- livestock_benefits
-    
     net_benefits <- total_benefits - intervention_cost
-    
     result_n_interv <- net_benefits}
   
     } #close intervention loop bracket
@@ -145,127 +150,44 @@ NPV_interv <-
 NPV_n_interv <-
   discount(result_n_interv, discount_rate, calculate_NPV = TRUE)
 
+# Beware, if you do not name your outputs (left-hand side of the equal sign) in the return section, 
+# the variables will be called output_1, _2, etc.
+
 return(list(Interv_NPV = NPV_interv,
-       NO_Interv_NPV = NPV_n_interv,
-       Cashflow_interv = result_interv,
-       Cashflow_n_interv = result_n_interv))
+            NO_Interv_NPV = NPV_n_interv,
+            NPV_decision_do = NPV_interv - NPV_n_interv,
+            Cashflow_decision_do = result_interv - result_n_interv))
 }
 
-# Running the model ####
 
-# This uses mcSimulation function instead of decisionSupport.
-# Be aware of using 'estimate_read_csv' for importing the estimates.
-# 'p', the output of the function, is a list having the elements y, x and call. We do not know
-# why the colnames in y are output_1 and _2 respectively.
-# It seems that the functionSyntax argument is not working well.
-
-
+## ----mcSimulation------------------------------------------------------------------------------------------------------------------------------------
 test_mcSimulation_function <- decisionSupport::mcSimulation(
-  estimate = decisionSupport::estimate_read_csv("vignettes/vignette_mcSimulation/example_input_table.csv"),
-  model_function = example_decision_function,
-  numberOfModelRuns = 1e4, #run 10,000 times
-  functionSyntax = "plainNames"
-)
-
-plot_distributions(mcSimulation_object = test_mcSimulation_function, vars = c("Interv_NPV", "NO_Interv_NPV"),
-                   method = 'boxplot_density',
-                   y_axis_name = "Hi",
-                   axis.title.x = ggplot2::element_text(size = 15, family = "serif"))
-
-plot_cashflow(mcSimulation_object = test_mcSimulation_function, cashflow_var_name = c("Cashflow_interv",
-                                                                                      "Cashflow_n_interv"),
-              axis.title.x = ggplot2::element_text(size = 15, family = "serif"))
-
-
-
-############################ Until this part is the main thing for the vignette #####################
-
-
-# Look for the class of the mcSimulation output
-
-class(test_mcSimulation_function)
-
-str(test_mcSimulation_function)
-
-
-
-
-
-
-# Here we used the welfare function
-
-
-test_welfare_function <- decisionSupport::welfareDecisionAnalysis(
-  estimate = decisionSupport::estimate_read_csv("vignettes/vignette_mcSimulation/example_input_table.csv"),
-  welfare = example_decision_function,
-  numberOfModelRuns = 1e4, #run 10,000 times
-  functionSyntax = "plainNames"
-)
-
-
-# Here the decision support function
-
-decisionSupport::decisionSupport(
-  inputFilePath = "vignettes/vignette_mcSimulation/example_input_table.csv",
-  outputPath = "vignettes/vignette_mcSimulation/test_results",
-  welfareFunction = example_decision_function,
-  numberOfModelRuns = 1e4, #run 10,000 times
-  functionSyntax = "plainNames",
-  write_table = TRUE
-)
-
-
-
-mcSimulationResults <- read.csv("vignettes/vignette_mcSimulation/test_results/mcSimulationResults.csv")
-
-names(mcSimulationResults)
-
-
-
-
-
-
-
-
-
-
-test_mcSimulation_function_2 <- mcSimulation(
-  estimate = decisionSupport::estimate_read_csv("vignettes/vignette_mcSimulation/example_input_table.csv"),
+  estimate = decisionSupport::estimate_read_csv("example_input_table.csv"),
   model_function = example_decision_function,
   numberOfModelRuns = 1e4, #run 10,000 times
   functionSyntax = "plainNames"
 )
 
 
-class(test_mcSimulation_function_2) == "mcSimulation"
+## ----------------------------------------------------------------------------------------------------------------------------------------------------
+decisionSupport::plot_distributions(mcSimulation_object = test_mcSimulation_function, 
+                                    vars = c("Interv_NPV", "NO_Interv_NPV"),
+                                    method = 'smooth_simple_overlay')
 
+## ----------------------------------------------------------------------------------------------------------------------------------------------------
+decisionSupport::plot_distributions(mcSimulation_object = test_mcSimulation_function, 
+                                    vars = "NPV_decision_do",
+                                    method = 'boxplot_density')
 
+## ----------------------------------------------------------------------------------------------------------------------------------------------------
+plot_cashflow(mcSimulation_object = test_mcSimulation_function, cashflow_var_name = "Cashflow_decision_do")
 
+## ----------------------------------------------------------------------------------------------------------------------------------------------------
+pls_result <- plsr.mcSimulation(object = test_mcSimulation_function,
+                  resultName = names(test_mcSimulation_function$y)[3], ncomp = 1)
 
+## ----------------------------------------------------------------------------------------------------------------------------------------------------
+input_table <- read.csv("example_input_table.csv")
 
-mcSimulation_DF <- data.frame(test_mcSimulation_function$y,
-                              test_mcSimulation_function$x)
-
-
-names(mcSimulation_DF)
-
-
-
-
-
-
-plot_distributions(mcSimulation_object = test_mcSimulation_function, vars = c("Interv_NPV", "NO_Interv_NPV"),
-                   method = 'boxplot_density',
-                   y_axis_name = "Hi",
-                   axis.title.x = ggplot2::element_text(size = 15, family = "serif"))
-
-
-
-
-class(test_mcSimulation_function)[[1]]
-
-
-plot_cashflow(mcSimulation_object = test_mcSimulation_function, cashflow_var_name = c("Cashflow_interv",
-                                                                                      "Cashflow_n_interv"))
-
+plot_pls(pls_result, input_table = input_table)
 
