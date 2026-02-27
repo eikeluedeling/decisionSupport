65749c95e3400c38a74472223545836315cb0f73
Katja Schiffers <kschiffe@uni-bonn.de>
2026-02-27 10:50:26 +0100

    chore: remove build artifacts, fix T → TRUE
    - Removed doc/ directory containing committed LaTeX build artifacts
      (PDF manual, .aux, .bbl, .log, etc.)
    - Fixed reverse=T → reverse=TRUE in plot_cashflow.R
      (T is not a reserved word and can be overwritten)

--- R/plot_cashflow.R
+++ R/plot_cashflow.R
@@ -192,7 +192,7 @@ plot_cashflow <- function(mcSimulation_object, cashflow_var_name,
     ggplot2::scale_fill_manual(values = c(color_25_75,  color_5_95)) +
     ggplot2::scale_color_manual(values = color_median) +
     ggplot2::guides(fill = 
-                      ggplot2::guide_legend(reverse=T, order = 1)) +
+                      ggplot2::guide_legend(reverse=TRUE, order = 1)) +
     ggplot2::labs(x= x_axis_name, y= y_axis_name, fill = legend_name, color = "") +
     ggplot2::facet_wrap(~ factor(decision_option, levels = cashflow_var_name, labels = facet_labels)) +
     ggplot2::theme_bw(base_size = base_size) +
